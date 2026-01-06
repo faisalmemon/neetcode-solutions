62edf1ab4a5b811e9c281fa4bff854d965b9fb2d
Faisal Memon <faisalmemonuk@yahoo.com>
2026-01-06 22:18:04 +0000

    Improve variable names.

--- 012_daily_temp.py
+++ 012_daily_temp.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         n = len(temperatures)
         res = [0] * n
@@ -7,4 +7,33 @@ class Solution:
                 if temperatures[j] > temperatures[i]:
                     res[i] = j - i
                     break
+        return res
+
+class Solution2:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        res = [0] * n
+        stack = [] # Stores indices
+
+        for i, temp in enumerate(temperatures):
+            # While the current temp is warmer than the temp at the stack's top index
+            while stack and temp > temperatures[stack[-1]]:
+                prev_index = stack.pop()
+                res[prev_index] = i - prev_index
+            stack.append(i)
+            
+        return res
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        res = [0] * n
+        index_stack = [] # Stores indices
+
+        for (index, current_temp) in enumerate(temperatures):
+            while index_stack and current_temp > temperatures[index_stack[-1]]:
+                previous_index = index_stack.pop()
+                res[previous_index] = index - previous_index
+            index_stack.append(index)
+            
         return res
